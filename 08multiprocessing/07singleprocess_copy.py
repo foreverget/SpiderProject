f618a7bb2d7acd92f9cde17f785dc4f9881897e6
lixiangsanxun <534551900@qq.com>
2018-09-30 05:16:16 +0800

    单进程实现文件拷贝

---
+++ 08multiprocessing/07singleprocess_copy.py
@@ -0,0 +1,34 @@
+
+
+
+# 定义拷贝文件的函数
+import os
+
+import time
+
+source = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'files')
+dest = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tofiles')
+
+
+def copy_file(source, dest):
+    old_file = open(source, 'rb')
+    new_file = open(dest, 'wb')
+    # 读取内容
+    content = old_file.read()
+    # 写入新文件。
+    new_file.write(content)
+    # 关闭文件。
+    old_file.close()
+    new_file.close()
+
+
+if __name__ == '__main__':
+    # 读出files目录下的所有文件。
+    file_list = os.listdir(source)
+    # 循环拷贝文件。
+    start = time.time()
+    for file_name in file_list:
+        copy_file(os.path.join(source, file_name), os.path.join(dest, file_name))
+    end = time.time()
+
+    print('耗费时间%.2f' % (end-start))
