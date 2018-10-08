e7c2e4d61af12a924bc7cea2f3dd2ae7ffeaa96b
lixiangsanxun <534551900@qq.com>
2018-10-08 21:46:07 +0800

    request-post的基本用法

--- 02requests/02post.py
+++ 02requests/02post.py
@@ -16,3 +16,22 @@ data = {
 resp = requests.post(url,headers=headers,data=data)
 # 如果是json数据，直接可以调用json方法
 print(resp.json())
+
+# 练习2
+
+import requests
+
+
+url = 'http://fanyi.baidu.com/sug'
+
+data = {
+    'kw': 'hello'
+}
+
+r = requests.post(url=url, data=data)
+
+print(r.text)
+
+# 把json数据编码
+print(r.json())
+
