25ff2aaed13bf69194f4d32a90f41c8371206378
lixiangsanxun <534551900@qq.com>
2018-10-08 22:05:52 +0800

    request-daili的基本用法介绍

--- 02requests/03proxy.py
+++ 02requests/03proxy.py
@@ -13,3 +13,23 @@ proxy = {
 resp = requests.get(url,headers=headers,proxies=proxy)
 with open('xx.html','w',encoding='utf-8') as fp:
     fp.write(resp.text)
+
+
+# 练习2
+import requests
+
+url = 'http://www.baidu.com/s'
+
+data = {
+    'wd': 'ip'
+}
+
+proxies = {
+    'http': '115.153.173.238:61234'
+}
+
+r = requests.get(url=url, params=data, proxies=proxies)
+r.encoding = 'utf-8'
+
+with open('./baiduip.html', 'w', encoding='utf-8') as fp:
+    fp.write(r.text)
