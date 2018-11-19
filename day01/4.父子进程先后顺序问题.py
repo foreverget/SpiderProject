0e5c860e51ffbd135fd237054b55f00fa4d7d08f
lixiang30 <534551900@qq.com>
2018-11-19 22:45:07 +0800

    父子进程先后顺序问题

---
+++ day01/4.父子进程先后顺序问题.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+from time import sleep
+import os
+
+#　子进程
+def run(self):
+    print('子进程开始执行--%s'%(os.getppid(),))
+    sleep(10)
+    print('子进程执行结束')
+
+if __name__ == '__main__':
+    print('父进程开始执行--%s'%(os.getppid(),))
+    # 创建子进程
+    p = Process(target=run)
+    p.start()
+    p.join(timeout=5)
+
+    print('父进程执行结束')
