ec402951ced62489b75073cf834ed02dfc604740
lixiang30 <534551900@qq.com>
2018-11-19 22:43:23 +0800

    单任务

---
+++ day01/1单任务.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+def run():
+    while True:
+        print("today is hot")
+        sleep(2)
+
+if __name__ == "__main__":
+
+    while True:
+        print("today is a good day ")
+        sleep(1)
+
+    run()
