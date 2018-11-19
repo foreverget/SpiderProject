adcc01f08be593c6bd1fb77a340d78e1eda343be
lixiang30 <534551900@qq.com>
2018-11-19 22:53:23 +0800

    进程池案例

---
+++ day01/6.进程池.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+import time
+import random
+
+def run(id):
+    print('子进程%d开始执行'%(id))
+    start = time.time()
+    time.sleep(random.choice([1,2,3]))
+    end = time.time()
+    print('子进程%d结束,耗时%f'%(id,(end-start)))
+
+if __name__ == '__main__':
+    print('主进程开始执行')
+    # 创建进程池
+    pp = Pool(5)
+    # 往进程池中添加进程
+    for i in range(6):
+        pp.apply_async(run,args=(i,))
+    # 关闭进程池
+    pp.close()
+    # 等待进程池中的进程结束
+    pp.join()
+    print('主进程结束')
