ab79d7797a85282f39010cedf07098c827ebc1e1
lixiang30 <534551900@qq.com>
2018-12-09 21:16:35 +0800

    requests post

---
+++ day02/requests_post.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://www.lagou.com/jobs/positionAjax.json?city=%E6%B7%B1%E5%9C%B3&needAddtionalResult=false&isSchoolJob=0"
+headers = {
+ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+ 'Referer': 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput='
+}
+data = {
+ 'first': 'true',
+ 'pn': 1,
+ 'kd': 'python'
+}
+resp = requests.post(url,headers=headers,data=data)
+# 如果是json数据，直接可以调用json方法
+print(resp.json())
